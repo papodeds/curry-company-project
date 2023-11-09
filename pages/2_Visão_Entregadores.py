1366fae6ba014a17eea1f9f1c38721149b700b4f
Pedro Castro <102838408+papodeds@users.noreply.github.com>
2023-11-09 13:04:04 -0300

    Update 2_Visão_Entregadores.py

--- pages/2_Visão_Entregadores.py
+++ pages/2_Visão_Entregadores.py
@@ -158,7 +158,7 @@ df1 = df1.loc[linhas_selecionadas, :]
 
 st.markdown('# Marketplace - Visão Entregadores')
 
-st.header('Visão Gerencial', divider='orange')
+st.header('Visão Gerencial')
 
 with st.container():
     st.markdown('## Métricas Gerais:') 
